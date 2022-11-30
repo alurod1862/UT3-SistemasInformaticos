0055eb52e6a4f530f4c64ce3675a0571b506ab1e
alurod1862 <91328330+alurod1862@users.noreply.github.com>
2022-12-01 00:57:35 +0100

    Create politicacontraseñas.ps1

---
+++ politicacontraseñas.ps1
@@ -0,0 +1,7 @@
+net accounts /lockoutthreshold:5
+net accounts /lockoutduration:60
+net accounts /lockoutwindow:60
+net accounts /maxpwage:30
+net accounts /minpwage:1
+net accounts /minpwlen:10
+net accounts 
