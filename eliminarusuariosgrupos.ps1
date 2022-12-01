fc64ed02fb38c589f78bcc395f85618dc9a96d65
alurod1862 <91328330+alurod1862@users.noreply.github.com>
2022-12-01 01:46:14 +0100

    Update and rename eliminarusuarios.ps1 to eliminarusuariosgrupos.ps1

--- eliminarusuariosgrupos.ps1
+++ eliminarusuariosgrupos.ps1
@@ -4,3 +4,8 @@ foreach  ($user in $usuarios)
     $clave=ConvertTo-SecureString $user.password -AsPlainText -Force
 	Remove-LocalUser $user.cuenta
 }
+
+$file_groups=Import-Csv -Path Grupos.csv 
+foreach ($group in $file_groups) { 
+  Remove-LocalGroup $group.nombre
+}
